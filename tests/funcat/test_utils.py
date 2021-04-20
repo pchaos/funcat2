9855ad92a817729aa4e0a273b3ecb6dacd75eb83
pchaos <drifthua@gmail.com>
2021-04-20 16:52:31 +0800

    update test_utils.py

--- tests/funcat/test_utils.py
+++ tests/funcat/test_utils.py
@@ -1,10 +1,14 @@
-from unittest import TestCase
+# -*- coding: utf-8 -*-
+import unittest
 from funcat.utils import *
 
 
-class TestUtils(TestCase):
+class TestUtils(unittest.TestCase):
     def test_get_str_date_from_int(self):
         data = get_str_date_from_int(20210101)
         self.assertTrue(data == '2021-01-01')
         data = get_str_date_from_int(20211011)
         self.assertTrue(data == '2021-10-11')
+
+if __name__ == '__main__':
+    unittest.main()
