f08d5a3b7cf3adab89bf08054d00e57f51afc1b3
pchaos <drifthua@gmail.com>
2021-04-20 10:59:04 +0800

    update test_get_trading_dates

--- tests/funcat/data/test_quantaxis_backend.py
+++ tests/funcat/data/test_quantaxis_backend.py
@@ -22,8 +22,12 @@ class TestQuantaxisDataBackend(TestCase):
 
     def test_get_trading_dates(self):
         data = self.qdb.get_trading_dates(20200101, 20210301)
-        self.assertTrue(len(data) > 250, f"股票代码数量：{len(data)},实际数量应该大于3000只。")
+        self.assertTrue(len(data) > 250, f"交易日期数量：{len(data)},实际应该大于250天。")
         print(f"交易日期：{data}")
+
+        data2 = self.qdb.get_trading_dates(20200101, 20210401)
+        self.assertTrue(len(data2) > len(data), f"交易日期数量：{len(data)}， {len(data2)},实际天数应该大于前一个交易天数")
+
 
 
 if __name__ == '__main__':
