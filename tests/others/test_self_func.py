3aa636a8d6f867aa92b0d4630e912f3da1c9dab8
pchaos <drifthua@gmail.com>
2021-05-13 16:43:45 +0800

    refractor test_talib.py rtest_self_func.py

--- tests/others/test_self_func.py
+++ tests/others/test_self_func.py
@@ -2,12 +2,13 @@
 import unittest
 import talib
 
+
 class AClass():
     func = 1
     func2 = talib.MA
 
     def __init__(self, arg):
-        print(self.func, arg)
+        print(f"in class:{type(self).__name__} --> ", self.func, arg)
 
     def a(self):
         print(self.func)
@@ -23,11 +24,12 @@ class BClass(AClass):
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_something(self):
         A = AClass
         B = BClass
-        print(A(3).a())
-        print(B(4).a())
+        print(f"  A(3).a() --> {A(3).a()}")
+        print(f"  A(4).a() --> {A(4).a()}")
         self.assertFalse(A(1).a() == B(1).a())
         self.assertFalse(A(1).getfunc2() == B(1).getfunc2())
         print(A(1).getfunc2(), B(1).getfunc2())
---
+++ tests/others/test_talib.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import unittest
+import numpy as np
+import talib
+import matplotlib.pyplot as plt
+from funcat.utils import handle_numpy_warning
+
+@handle_numpy_warning
+def f(x):
+    return np.int(x)
+
+
+class MyTestCase(unittest.TestCase):
+    @classmethod
+    def setUp(self) -> None:
+        self.close = np.random.random(100)
+
+    def test_ma(self):
+        close = self.close
+        print(f"数据类型：{type(close)}")
+        output = talib.SMA(close)
+        print(f"输出数据类型：{type(output)}")
+        print(output-close)
+
+    def test_raiseexception(self):
+        x = np.arange(1, 15.1, 0.1)
+        with self.assertRaises(TypeError):
+            # look at the call signature of np.int
+            f(x)
+
+    def test_raiseexception_2(self):
+        x = np.arange(1, 15.1, 0.1)
+        f2 = np.vectorize(f)
+
+        plt.plot(x, f2(x))
+        plt.show()
+
+if __name__ == '__main__':
+    unittest.main()
