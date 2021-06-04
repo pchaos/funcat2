0859e44a6d91c49048fbbddc439253daaca10e12
pchaos <drifthua@gmail.com>
2021-06-04 10:21:32 +0800

    update delist

--- tests/others/test_delist.py
+++ tests/others/test_delist.py
@@ -20,6 +20,42 @@ class TestDeList(FuncatTestCase):
         print(df)
         print(df[["code", "LISTING_DATE", "CHANGE_DATE"]])
 
+    def test_stock_info(self):
+        df = DeList.stock_info()
+        print(df.head(20))
+        print(f"code counts:{len(df)}")
+
+    def test_stock_info_sh_name_code(self):
+        df = DeList.stock_info_sh_name_code()
+        self.assertTrue(len(df) > 1500, f"stock_info_sh_name_code: {len(df)}")
+        print(df.head(10))
+
+    def test_stock_info_sh_name_code_delist(self):
+        df = DeList.stock_info_sh_name_code()
+        df_delist = DeList.stock_info_sh_delist()
+        codelist = df['code'].tolist()
+        codelist_delist = df_delist['code'].tolist()
+        print(codelist_delist)
+        for i in range(len(codelist_delist)):
+            self.assertFalse(
+                codelist_delist[i] in codelist, f"{codelist_delist[i]}")
+
+    def test_stock_info_sz_name_code(self):
+        df = DeList.stock_info_sz_name_code()
+        self.assertTrue(len(df) > 2000, f"stock_info_sh_name_code: {len(df)}")
+
+    def test_stock_info_code(self):
+        df1 = DeList.stock_info_sh_name_code()
+        df2 = DeList.stock_info_sz_name_code()
+        df = df1.append(df2)
+        self.assertTrue(len(df) == len(df1) + len(df2),
+                        f"{len(df)} == {len(df1)} + {len(df2)}")
+        print(f"{len(df)} == {len(df1)} + {len(df2)}")
+
+    def test_(self):
+        """list_status    str    N    上市状态 L上市 D退市 P暂停上市，默认是L"""
+        ts.pro_api().stock_basic(exchange='', list_status='D').set_index('symbol', drop=True)
+
 
 if __name__ == '__main__':
     FuncatTestCase.main()
