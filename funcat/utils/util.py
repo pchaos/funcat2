965ca1d01ec0d66ed87f4e2e24864f1eba2895e3
pchaos <drifthua@gmail.com>
2021-06-06 10:59:27 +0800

    add fft_denoiser

--- funcat/utils/util.py
+++ funcat/utils/util.py
@@ -103,7 +103,7 @@ import numba
 
 
 @numba.njit
-def shift(arr:np.array, num:int, fill_value=np.nan):
+def shift(arr: np.array, num: int, fill_value=np.nan):
     """Shift elements in a numpy array
     https://stackoverflow.com/questions/30399534/shift-elements-in-a-numpy-array
     """
@@ -128,6 +128,53 @@ def rolling_apply(fun, a, w):
 
 
 # @numba.njit
-def rolling_sum(arr:np.array, n=4) -> np.array:
+def rolling_sum(arr: np.array, n=4) -> np.array:
     pre_sum = np.convolve(arr, np.ones(n, dtype=int), 'valid')
     return np.append(np.array([np.nan] * (n - 1)), pre_sum)
+
+
+def fft_denoiser(x, n_components, to_real=True):
+    """Fast fourier transform denoiser.
+
+    Denoises data using the fast fourier transform.
+
+    Parameters
+    ----------
+    x : numpy.array
+        The data to denoise.
+    n_components : int
+        The value above which the coefficients will be kept.
+    to_real : bool, optional, default: True
+        Whether to remove the complex part (True) or not (False)
+
+    Returns
+    -------
+    clean_data : numpy.array
+        The denoised data.
+
+    References
+    ----------
+    .. [1] Steve Brunton - Denoising Data with FFT[Python]
+       https://www.youtube.com/watch?v=s2K1JfNR7Sc&ab_channel=SteveBrunton
+        https://medium.com/swlh/5-tips-for-working-with-time-series-in-python-d889109e676d
+    """
+    n = len(x)
+
+    # compute the fft
+    fft = np.fft.fft(x, n)
+
+    # compute power spectrum density
+    # squared magnitud of each fft coefficient
+    PSD = fft * np.conj(fft) / n
+
+    # keep high frequencies
+    _mask = PSD > n_components
+    fft = _mask * fft
+
+    # inverse fourier transform
+    clean_data = np.fft.ifft(fft)
+
+    if to_real:
+        clean_data = clean_data.real
+
+    return clean_data
