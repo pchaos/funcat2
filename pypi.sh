ba74863ccb2437c01573b9143b3bd0f86af1573e
pchaos <drifthua@gmail.com>
2021-06-15 09:42:17 +0800

    add pypi.sh

---
+++ pypi.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Generate pypi wheels universal package and upload
+#
+# python setup.py bdist_wheel --universal upload -r pypitest
+python setup.py bdist_wheel --universal upload -r pypi
