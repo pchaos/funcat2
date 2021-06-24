ed3e2f6eec8e45e006140b92195d3bd370f38eae
pchaos <drifthua@gmail.com>
2021-06-24 19:18:32 +0800

    update

--- run/run.sh
+++ run/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 10日卡夫曼自适应均线选股
-pytest -v  "../tests/funcat/test_ema_trends.py"::Test_ema_trend::test_condition_kama_ema3
+pytest -v  "../tests/funcat/test_ema_trends.py"::Test_ema_trend::test_condition_kama_ema3 && vim /tmp/kama_ema_*.txt
