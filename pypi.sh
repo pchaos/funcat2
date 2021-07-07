28aaa1508c826ca4d693766187d1b005977d46ab
pchaos <drifthua@gmail.com>
2021-07-07 12:34:28 +0800

    update

--- pypi.sh
+++ pypi.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-#
+
 # Generate pypi wheels universal package and upload
-#
+
+# 以下方式不支持markdown格式说明上传。
 # python setup.py bdist_wheel --universal upload -r pypitest
-python setup.py bdist_wheel --universal upload -r pypi
+# python setup.py bdist_wheel --universal upload -r pypi
+
+# 以下方式支持markdown格式说明上传。
+python setup.py sdist
+python setup.py bdist_wheel
+
+twine upload --repository funcat2 dist/funcat2-*-py3-none-any.whl -r pypi  --verbose
+
 rm -rf build
--- setup.py
+++ setup.py
@@ -18,14 +18,25 @@ with open(join(dirname(__file__), 'funcat/VERSION.txt'), 'rb') as f:
 
 try:
     # AttributeError: 'ParsedRequirement' object has no attribute 'req'; for pip >=20.1
-    req = [str(ir.requirement) for ir in parse_requirements("requirements.txt", session=False)],
+    req = [
+        str(ir.requirement)
+        for ir in parse_requirements("requirements.txt", session=False)
+    ],
+    with open(join(dirname(__file__), 'README.md'), 'r', encoding='utf-8') as f:
+        long_description = f.read()
 except:
-    req = [str(ir.req) for ir in parse_requirements("requirements.txt", session=False)],
+    req = [
+        str(ir.req)
+        for ir in parse_requirements("requirements.txt", session=False)
+    ],
 
 setup(
     name='funcat2',
     version=version,
-    description='Funcat2保持与funcat兼容;Funcat 将同花顺、通达信、文华财经等的公式移植到了 Python 中。同花顺、通达信、文华财经麦语言等公式的表达十分简洁，适合做技术分析。苦于 Python 缺乏这种领域特定语言的表达能力，所以用 Python 基于 numpy 实现了一套。Funcat2增加QUANTAXIS的支持',
+    description=
+    'Funcat2保持与funcat兼容;Funcat 将同花顺、通达信、文华财经等的公式移植到了 Python 中。同花顺、通达信、文华财经麦语言等公式的表达十分简洁，适合做技术分析。苦于 Python 缺乏这种领域特定语言的表达能力，所以用 Python 基于 numpy 实现了一套。Funcat2增加QUANTAXIS的支持',
+    long_description_content_type="text/markdown", 
+    long_description=long_description,
     packages=find_packages(exclude=[]),
     author='Hua Liang, p19992003',
     url='https://github.com/pchaos/funcat2',
@@ -41,5 +52,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
