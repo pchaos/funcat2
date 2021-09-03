ff554cc134906a5a182fc31774488d62a839b314
pchaos <drifthua@gmail.com>
2021-09-03 09:57:15 +0800

    update

--- tests/others/test_backtrader1.py
+++ tests/others/test_backtrader1.py
@@ -12,7 +12,7 @@ from datetime import datetime
 # 正常显示画图时出现的中文和负号
 from pylab import mpl
 
-__updated__ = "2021-06-16"
+__updated__ = "2021-06-25"
 
 
 class my_strategy1(bt.Strategy):
@@ -49,10 +49,6 @@ class my_strategy1(bt.Strategy):
                 self.order = self.sell(size=500)
 
 
-mpl.rcParams['axes.unicode_minus'] = False
-mpl.rcParams['font.sans-serif'] = ['SimHei']
-
-
 def get_data(code, start='2010-01-01', end='2020-03-31'):
     df = ts.get_k_data(code, autype='qfq', start=start, end=end)
     df.index = pd.to_datetime(df.date)
@@ -61,35 +57,39 @@ def get_data(code, start='2010-01-01', end='2020-03-31'):
     return df
 
 
-dataframe = get_data('600000')
+if __name__ == '__main__':
 
-# 回测期间
-start = datetime(2010, 1, 31)
-end = datetime(2020, 3, 31)
-# 加载数据
-data = bt.feeds.PandasData(dataname=dataframe, fromdate=start, todate=end)
-# 初始化cerebro回测系统设置
-cerebro = bt.Cerebro()
-# 将数据传入回测系统
-cerebro.adddata(data)
-# 将交易策略加载到回测系统中
-cerebro.addstrategy(my_strategy1)
-# 设置初始资本为10,000
-startcash = 10000
-cerebro.broker.setcash(startcash)
-# 设置交易手续费为 0.2%
-cerebro.broker.setcommission(commission=0.002)
+    mpl.rcParams['axes.unicode_minus'] = False
+    mpl.rcParams['font.sans-serif'] = ['SimHei']
+    dataframe = get_data('600000')
 
-d1 = start.strftime('%Y%m%d')
-d2 = end.strftime('%Y%m%d')
-print(f'初始资金: {startcash}\n回测期间：{d1}:{d2}')
-# 运行回测系统
-cerebro.run()
-# 获取回测结束后的总资金
-portvalue = cerebro.broker.getvalue()
-pnl = portvalue - startcash
-# 打印结果
-print(f'净收益: {round(pnl,2)}')
-print(f'总资金: {round(portvalue,2)}')
+    # 回测期间
+    start = datetime(2010, 1, 31)
+    end = datetime(2020, 3, 31)
+    # 加载数据
+    data = bt.feeds.PandasData(dataname=dataframe, fromdate=start, todate=end)
+    # 初始化cerebro回测系统设置
+    cerebro = bt.Cerebro()
+    # 将数据传入回测系统
+    cerebro.adddata(data)
+    # 将交易策略加载到回测系统中
+    cerebro.addstrategy(my_strategy1)
+    # 设置初始资本为10,000
+    startcash = 10000
+    cerebro.broker.setcash(startcash)
+    # 设置交易手续费为 0.2%
+    cerebro.broker.setcommission(commission=0.002)
 
-cerebro.plot(style='candlestick')
+    d1 = start.strftime('%Y%m%d')
+    d2 = end.strftime('%Y%m%d')
+    print(f'初始资金: {startcash}\n回测期间：{d1}:{d2}')
+    # 运行回测系统
+    cerebro.run()
+    # 获取回测结束后的总资金
+    portvalue = cerebro.broker.getvalue()
+    pnl = portvalue - startcash
+    # 打印结果
+    print(f'净收益: {round(pnl,2)}')
+    print(f'总资金: {round(portvalue,2)}')
+
+    cerebro.plot(style='candlestick', width=30, height=20, iplot=False)
